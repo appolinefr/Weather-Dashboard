7f30ed1e13889f6c603c4ceaa9031fc2da2c8d53
Appoline Cogan <appolinecogan@gmail.com>
2022-07-14 12:27:22 +0700

    updated forecast display and added class for uvindex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
 //variables for forminput and button
 var searchFormInput = document.getElementById("citySearchForm");
 var searchBtn = document.getElementById("searchBtn");
-var mainWeather = document.getElementById("mainWeather");
 var forecastContainer = document.getElementById("forecast");
-var forecastColumn = document.getElementById("forecastColumn");
 
 //function facilitating the event listener on the search button
 var searchSubmitHandler = function (event) {
@@ -13,10 +11,8 @@ var searchSubmitHandler = function (event) {
   var city = searchFormInput.value.trim();
   var date = moment().format("DD/ MM/YYYY");
   if (city) {
-    var cityName = document.createElement("h2");
+    var cityName = document.querySelector(".city");
     cityName.textContent = city + " " + date;
-    mainWeather.appendChild(cityName);
-
     getWeather(city);
 
     searchFormInput.value = "";
@@ -54,7 +50,6 @@ function getWeather(city) {
         if (response.ok) {
           response.json().then(function (data) {
             displayWeather(data);
-            displayForecast(data);
           });
         } else {
           return;
@@ -65,41 +60,55 @@ function getWeather(city) {
 
 // function to display current weather
 var displayWeather = function (data) {
-  //element for icon not working for now
-  // icon = document.createElement("h3");
-  // var currentIcon = data.current.weather[0].icon;
-  // icon.textContent = currentIcon;
-  // mainWeather.appendChild(icon);
-
-  // creating ul to hold list elements
-  var weatherList = document.createElement("ul");
-  mainWeather.appendChild(weatherList);
   console.log(data);
-  // creating elements for temperature
-  var temperature = document.createElement("li");
-  currentTemperature = Math.round(data.current.temp);
-  temperature.textContent = "Temperature: " + currentTemperature + "°";
-  weatherList.appendChild(temperature);
+  var temperature = document.querySelector(".temp");
+  temperature.textContent =
+    "Temperature: " + Math.round(data.current.temp) + "°";
 
   // creating elements for humidity
-  var humidity = document.createElement("li");
-  currentHumidity = Math.round(data.current.humidity);
-  humidity.textContent = "Humidity: " + currentHumidity + "%";
-  weatherList.appendChild(humidity);
+  var humidity = document.querySelector(".humidity");
+  humidity.textContent = "Humidity: " + Math.round(data.current.humidity) + "%";
 
   // creating elements for wind
-  var wind = document.createElement("li");
-  currentWind = Math.round(data.current.wind_speed);
-  wind.textContent = "Wind: " + currentWind + "KM/H";
-  weatherList.appendChild(wind);
+  var wind = document.querySelector(".wind");
+  wind.textContent = "Wind: " + Math.round(data.current.wind_speed) + "KM/H";
 
   // creating elements for uvIndex
-  var uvIndex = document.createElement("li");
-  currentuvIndex = Math.round(data.current.uvi);
-  uvIndex.textContent = "UV Index: " + currentuvIndex;
-  weatherList.appendChild(uvIndex);
+  var uvIndex = document.querySelector(".uv");
+  uvIndex.textContent = "UV Index: " + Math.round(data.current.uvi);
 
-  // Need to add function for uv index color change
+  if (uvIndex < 3) {
+    //if uv index is low (1-2)
+    uvIndex.setAttribute("class", "bgColor: bg-success", "color: text-light ");
+  } else if (uvIndex > 2 && uvIndex < 6) {
+    //if uv index is mocerate (3-5)
+    uvIndex.setAttribute("class", "bgColor: bg-warning", "color: text-dark ");
+  } //if uv index is high (6+)
+  else {
+    uvIndex.setAttribute("class", "bgColor: bg-danger", "color: text-light ");
+  }
+
+  for (let i = 0; i < 6; i++) {
+    let forecastColumn = document.createElement("div");
+    forecastColumn.setAttribute("class", "col");
+    forecastContainer.appendChild(forecastColumn);
+
+    let dateEl = document.createElement("p");
+    dateEl.textContent = data.daily[i].dt;
+    forecastColumn.appendChild(dateEl);
+    let iconEl = document.createElement("p");
+    iconEl.textContent = data.daily[i].weather[0].icon;
+    forecastColumn.appendChild(iconEl);
+    let humidityEl = document.createElement("p");
+    humidityEl.textContent = data.daily[i].humidity;
+    forecastColumn.appendChild(humidityEl);
+    let windEl = document.createElement("p");
+    windEl.textContent = data.daily[i].wind_speed;
+    forecastColumn.appendChild(windEl);
+    let tempEl = document.createElement("p");
+    tempEl.textContent = data.daily[i].temp.day;
+    forecastColumn.appendChild(tempEl);
+  }
 };
 
 // var displayForecast = function (data) {
@@ -133,28 +142,7 @@ var displayWeather = function (data) {
 // };
 
 searchBtn.addEventListener("click", searchSubmitHandler);
-//     let date = data.daily[i].dt;
-//     let icon = data.daily[i].weather[0].icon;
-//     let humidity = data.daily[i].humidity;
-//     let wind = data.daily[i].wind_speed;
-//     let temperature = data.daily[i].temp.day;
-//   }
-
-//   let dateEl = document.createElement("p");
-//   dateEl.textContent = date[i];
-//   forecastUl.appendChild(dateEl);
-//   let iconEl = document.createElement("p");
-//   iconEl.textContent = icon[i];
-//   forecastUl.appendChild(iconEl);
-//   let humidityEl = document.createElement("p");
-//   humidityEl.textContent = humidity[i];
-//   forecastUl.appendChild(humidityEl);
-//   let windEl = document.createElement("p");
-//   windEl.textContent = wind[i];
-//   forecastUl.appendChild(windEl);
-//   let tempEl = document.createElement("p");
-//   tempEl.textContent = temperature[i];
-//   forecastUl.appendChild(tempEl);
+//
 // };
 
 //     const forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
